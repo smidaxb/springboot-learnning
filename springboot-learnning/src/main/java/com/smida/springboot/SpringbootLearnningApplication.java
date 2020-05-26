636049275ffe9930966b4c47fa287f7cd5f31702
smidaxb <smidaxb@163.com>
2020-05-26 17:10:10 +0800

    init project

---
+++ springboot-learnning/src/main/java/com/smida/springboot/SpringbootLearnningApplication.java
@@ -0,0 +1,13 @@
+package com.smida.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearnningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearnningApplication.class, args);
+    }
+
+}
